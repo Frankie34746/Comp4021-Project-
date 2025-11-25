79c25d2d351cf59fb46f8cfa45706d1f0e7a31fe
NewPlayer00001 <155231428+Cytherusn@users.noreply.github.com>
2025-11-25 20:35:55 +0800

    Update server.js
    Currently stucking on register endpoint not found...

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
+const express = require("express");
 
+const bcrypt = require("bcryptjs");       // bcryptjs is used instead of bcrypt
+const fs = require("fs");
+
+// Create the Express app
+const app = express();
+
+// Use the 'public' folder to serve static files
+app.use(express.static("public"));
+
+// Use the json middleware to parse JSON data
+app.use(express.json());
+
+// Handle the /register endpoint
+app.post("/register", (req, res) => {
+    // Get the JSON data from the body
+    const { username } = req.body;
+
+    //
+    // D. Reading the users.json file
+    //
+    const users = JSON.parse(fs.readFileSync("./data/users.json"));
+
+    //
+    // G. Adding the new user account
+    //
+    users[username] = {username};
+
+    //
+    // H. Saving the users.json file
+    //
+    fs.writeFileSync("./data/users.json", JSON.stringify(users, null, " "));
+
+    //
+    // I. Sending a success response to the browser
+    //
+    res.json({ status: "success" })
+});
+
+app.listen(8000, () => {
+    console.log("Game Sever is running...");
+});
