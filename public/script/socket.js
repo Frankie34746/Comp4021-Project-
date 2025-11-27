3f5f8123fa306fbcc1d712b85ed4713fa92cc6a6
NewPlayer00001 <155231428+Cytherusn@users.noreply.github.com>
2025-11-27 16:53:53 +0800

    Create socket.js

---
+++ public/script/socket.js
@@ -0,0 +1,16 @@
+const Socket = (function() {
+    // This stores the current Socket.IO socket
+    let socket = null;
+
+    // This function gets the socket from the module
+    const getSocket = function() {
+        return socket;
+    };
+
+    // This function connects the server and initializes the socket
+    const connect = function() {
+        socket = io();
+    };
+
+    return { getSocket, connect };
+})();
