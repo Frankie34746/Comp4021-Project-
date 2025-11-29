a0e2171bb4a5aeb8a3cb9d9b58b3e9ca38f913c4
Frankie34746 <105794236+Frankie34746@users.noreply.github.com>
2025-11-29 18:29:17 +0800

    Implement bounding box method for gem
    Added bounding box calculation for the gem.

--- public/script/treasure.js
+++ public/script/treasure.js
@@ -5,13 +5,19 @@
 // - `color` - The colour of the gem
 const treasure = function(ctx, x, y, color) {
 
+    const sprite_height = 16;
+    const sprite_width = 16;
+    
+    const halfWidth = sprite_width / 2;
+    const halfheight = sprite_height / 2;
+
     // This is the sprite sequences of the gem of four colours
     // `green`, `red`, `yellow` and `purple`.
     const sequences = {
-        green:  { x: 192, y:  0, width: 16, height: 16, count: 4, timing: 200, loop: true },
-        red:    { x: 192, y: 16, width: 16, height: 16, count: 4, timing: 200, loop: true },
-        yellow: { x: 192, y: 32, width: 16, height: 16, count: 4, timing: 200, loop: true },
-        purple: { x: 192, y: 48, width: 16, height: 16, count: 4, timing: 200, loop: true }
+        green:  { x: 192, y:  0, width: sprite_width, height: sprite_height, count: 4, timing: 200, loop: true },
+        red:    { x: 192, y: 16, width: sprite_width, height: sprite_height, count: 4, timing: 200, loop: true },
+        yellow: { x: 192, y: 32, width: sprite_width, height: sprite_height, count: 4, timing: 200, loop: true },
+        purple: { x: 192, y: 48, width: sprite_width, height: sprite_height, count: 4, timing: 200, loop: true }
     };
 
     // This is the sprite object of the gem created from the Sprite module.
@@ -39,6 +45,18 @@ const treasure = function(ctx, x, y, color) {
         setColor(colors[Math.floor(Math.random() * 4)]);
     };
 
+    const getBoundingBox = function() {
+        let { x, y } = sprite.getXY();
+
+        let left = x-halfWidth;
+        let right = x+halfWidth;
+        let top = y-halfheight;
+        let bottom = y+sprite_height;
+
+        // Assuming BoundingBox takes ctx, top, left, bottom, right 
+        return BoundingBox(ctx, top, left, bottom, right);
+    };
+
     // Randomize initially
     randomize(gameArea);
 
@@ -47,7 +65,7 @@ const treasure = function(ctx, x, y, color) {
         getXY: sprite.getXY,
         setXY: sprite.setXY,
         setColor: setColor,
-        getBoundingBox: sprite.getBoundingBox,
+        getBoundingBox: getBoundingBox,
         randomize: randomize,
         draw: sprite.draw,
         update: sprite.update
